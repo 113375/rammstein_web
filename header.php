eda892aa6fe652fcb565dacf2ed499f2a4c79df9
Дима <knyazevdima05@gmail.com>
2021-01-25 20:08:16 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<ul class="modal-header header">
+    <li><span><a href="index.php">Главная</a></span></li>
+    <li><span><a href="#">О Rammstein</a></span></li>
+    <li><span><a href="#">Альбомы</a></span></li>
+    <li><span><a href="#">Тест</a></span></li>
+</ul>
+
+
+<?php
+?>
